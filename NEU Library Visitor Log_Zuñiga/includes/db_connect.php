edddffa5cd40b384338389344ab8d7a84c226b13
Clark Kent P. Zuñiga <clarkkent.zuniga@neu.edu.ph>
2026-03-18 07:43:51 +0800

    Update db_connect.php

--- NEU Library Visitor Log_Zuñiga/includes/db_connect.php
+++ NEU Library Visitor Log_Zuñiga/includes/db_connect.php
@@ -27,6 +27,8 @@ date_default_timezone_set('Asia/Manila');
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
+$conn->query("SET time_zone = '+08:00'");
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
